e4d56bbfe8f672009a8260f6781d63b7e569ec6b
chamika94 <chamikasandaruwan1994@gmail.com>
2022-09-06 22:53:12 +0530

    Implemented Consumer/Producer to pass String messages to the Kafka server using spring boot

---
+++ Learn-With-Krish/Implement MS Using Kafka/src/test/java/com/kafka/springtbootkafka/SpringtbootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.springtbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtbootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
