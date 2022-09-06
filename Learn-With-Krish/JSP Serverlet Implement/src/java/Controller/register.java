414713eed28e7200acf5c3737a9c1dc60089c58d
chamika94 <chamikasandaruwan1994@gmail.com>
2022-09-06 12:14:02 +0530

    Implemented the code to get data to the Serverlet from the HTTP request body

--- Learn-With-Krish/JSP Serverlet Implement/src/java/Controller/register.java
+++ Learn-With-Krish/JSP Serverlet Implement/src/java/Controller/register.java
@@ -3,6 +3,7 @@ package Controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+/*import java.sql.Connection;*/
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,61 +18,54 @@ public class register extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        try {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet register</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet register at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        } finally {
-            out.close();
-        }
+        
+        try(PrintWriter out = response.getWriter()) {
+
+        } 
     }
 
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        PrintWriter out = response.getWriter();
-        
-        String fullname = request.getParameter("fname");
-        String username = request.getParameter("uname");
-        String email = request.getParameter("email");
-        String password = request.getParameter("psw");
-        String confirmpass = request.getParameter("cnfpsw");
-        
-        try{
-            Connection con;
-            
-            Class.forName("com.mysql.jdbc.Driver");
-            
-            con = DriverManger.getConnection("jdbc:mysql://localhost:3306/","root","root");
-            
-            System.out.println("Connected");
-        }catch(Exception e){
-           System.out.println("ERROR : "+ e.getMessage());
-        }
+
         
         
     }
 
-  
-    
-    
-    
-    
-    
-    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+
+        
+            PrintWriter out = response.getWriter();
+
+            String fullname = request.getParameter("fname");
+            String username = request.getParameter("uname");
+            String email = request.getParameter("email");
+            String password = request.getParameter("psw");
+            String confirmpass = request.getParameter("cnfpsw");
+            
+            
+            System.out.println(fullname);
+            System.out.println(username);
+            System.out.println(email);
+            System.out.println(password);
+            System.out.println(confirmpass);
+
+            /*          try{
+            Connection con;
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            con = DriverManger.getConnection("jdbc:mysql://localhost:3306/","root","root");
+            
+            System.out.println("Connected");
+            }catch(Exception e){
+            System.out.println("ERROR : "+ e.getMessage());
+            }*/
+        
+        
     }
 
   
@@ -79,5 +73,8 @@ public class register extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
+ 
+    
 
 }
