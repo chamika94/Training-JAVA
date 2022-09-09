442180243efcabd880eb55073ea4e8e26a4c5cef
chamika94 <chamikasandaruwan1994@gmail.com>
2022-09-09 16:50:58 +0530

    Implemented a method to override Stsrt() method

--- Tread Implementation/TreadsImplementation/src/com/krish/Thread/Application.java
+++ Tread Implementation/TreadsImplementation/src/com/krish/Thread/Application.java
@@ -7,7 +7,6 @@ public class Application {
 
         Printer printer = new Printer();
         printer.start();
-
         for (int i=0; i<10;i++){
             System.out.println("This is from main thread"+currentThread().getName()+" "+i);
         }
--- Tread Implementation/TreadsImplementation/src/com/krish/Thread/Printer.java
+++ Tread Implementation/TreadsImplementation/src/com/krish/Thread/Printer.java
@@ -2,9 +2,12 @@ package com.krish.Thread;
 
 public class Printer extends Thread{
 
+    public void start(){
+        super.start();
+        System.out.println("This is start");
+    }
     @Override
     public void run() {
-
         for (int i=0; i<10;i++){
             System.out.println("This is from child thread"+currentThread().getName()+" "+i);
         }
