6f704c4efd1fa292c3751c123e931470467945de
chamika94 <chamikasandaruwan1994@gmail.com>
2022-09-10 23:27:06 +0530

    Implemented classes with relavent annotations

--- Spring Fundamentals/SpringTrainingJava/src/com/krishantha/training/salesmanager/service/EmployeeServiceImpl.java
+++ Spring Fundamentals/SpringTrainingJava/src/com/krishantha/training/salesmanager/service/EmployeeServiceImpl.java
@@ -2,37 +2,42 @@ package com.krishantha.training.salesmanager.service;
 
 import com.krishantha.training.salesmanager.repository.EmployeeRepository;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.krishantha.training.salesmanager.model.Employee;
 
 public class EmployeeServiceImpl implements EmployeeService  {
 	
-	
+	@Autowired
 	EmployeeRepository employeeRepository;
 	
-	
-	
-    public EmployeeServiceImpl() {
-		// TODO Auto-generated constructor stub
-		 System.out.println("default constructor executed");
-	}
     public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
 		// TODO Auto-generated constructor stub
     	
 		 System.out.println("overload constructor executed");
 		 this.employeeRepository = employeeRepository;	
 	}
+	
+    public EmployeeServiceImpl() {
+		// TODO Auto-generated constructor stub
+		 System.out.println("default constructor executed");
+	}
 
-	public void setEmployeeRepository(EmployeeRepository employeeRepository) {
-		 
-		 this.employeeRepository = employeeRepository;			 
-		 
-     }
-	 
 	 public EmployeeRepository getEmployeeRepository() {
 		 
 		 return employeeRepository;			 
 		 
      }
+	 
+	 @Autowired
+	public void setEmployeeRepository(EmployeeRepository employeeRepository) {
+		System.out.println("setter executed");
+		 this.employeeRepository = employeeRepository;			 
+		 
+     }
+	 
+
 	 
 	 public List<Employee> getAllEmployee(){
 		
