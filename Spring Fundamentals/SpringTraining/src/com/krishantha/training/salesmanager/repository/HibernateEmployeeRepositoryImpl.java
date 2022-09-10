a217e2741ba9a5020f8fb064c0feb1b3c70c38d9
chamika94 <chamikasandaruwan1994@gmail.com>
2022-09-10 14:09:12 +0530

    Created model/repository packages and Implemented relavent classes

--- Spring Fundamentals/SpringTraining/src/com/krishantha/training/salesmanager/repository/HibernateEmployeeRepositoryImpl.java
+++ Spring Fundamentals/SpringTraining/src/com/krishantha/training/salesmanager/repository/HibernateEmployeeRepositoryImpl.java
@@ -1,5 +1,22 @@
 package com.krishantha.training.salesmanager.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+import com.krishantha.training.salesmanager.model.Employee;
+
 public class HibernateEmployeeRepositoryImpl {
+	
+	public List<Employee> grtAllEmployees(){
+		
+		List<Employee> employees = new ArrayList<>();
+		
+		Employee employee = new Employee();
+		employee.setEmployeeLocation("Krishantha");
+		employee.setEmployeeName("Kadawatha");
+		employees.add(employee);
+		
+		return employees;
+		
+	}
 
 }
