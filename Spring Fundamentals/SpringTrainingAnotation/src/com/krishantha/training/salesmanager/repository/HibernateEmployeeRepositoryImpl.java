ecd2e374b7240c5739f5d269142fd85ec009ba00
chamika94 <chamikasandaruwan1994@gmail.com>
2022-09-10 22:34:23 +0530

    Added relavent annotations

--- Spring Fundamentals/SpringTrainingAnotation/src/com/krishantha/training/salesmanager/repository/HibernateEmployeeRepositoryImpl.java
+++ Spring Fundamentals/SpringTrainingAnotation/src/com/krishantha/training/salesmanager/repository/HibernateEmployeeRepositoryImpl.java
@@ -2,8 +2,12 @@ package com.krishantha.training.salesmanager.repository;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
 import com.krishantha.training.salesmanager.model.Employee;
 
+@Repository("employeeRepository")
 public class HibernateEmployeeRepositoryImpl implements EmployeeRepository {
 	
 	public List<Employee> getAllEmployees(){
--- Spring Fundamentals/SpringTrainingAnotation/src/com/krishantha/training/salesmanager/service/EmployeeServiceImpl.java
+++ Spring Fundamentals/SpringTrainingAnotation/src/com/krishantha/training/salesmanager/service/EmployeeServiceImpl.java
@@ -2,19 +2,31 @@ package com.krishantha.training.salesmanager.service;
 
 import com.krishantha.training.salesmanager.repository.EmployeeRepository;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.krishantha.training.salesmanager.model.Employee;
 
+@Service("employeeService")
 public class EmployeeServiceImpl implements EmployeeService  {
 	
 	
 	EmployeeRepository employeeRepository;
 	
-	public EmployeeServiceImpl(EmployeeRepository employeerepository) {
-		this.employeeRepository = employeerepository;
+	public EmployeeServiceImpl() {
+		System.out.println("Default constructore executted");
 	}
 	
+
+	public EmployeeServiceImpl( EmployeeRepository employeeRepository) {
+		System.out.println("Overloaded constructore executted");
+		this.employeeRepository = employeeRepository;
+	}
+	
+	@Autowired
 	 public void setEmployeeRepository(EmployeeRepository employeeRepository) {
-		 
+		System.out.println("Setter injection fired");
 		 this.employeeRepository = employeeRepository;			 
 		 
      }
