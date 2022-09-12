e2262d30d9a8da86ed4455e55174f2c2d04cab44
chamika94 <chamikasandaruwan1994@gmail.com>
2022-09-12 09:31:35 +0530

    Created basic Spring Boot web project

---
+++ SOAP Example/SpringBootSoapWebService/src/main/java/com/SpringBoot/SoapWebService/SpringBootSoapWebService/SpringBootSoapWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.SoapWebService.SpringBootSoapWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoapWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoapWebServiceApplication.class, args);
+	}
+
+}
---
+++ SOAP Example/SpringBootSoapWebService/src/test/java/com/SpringBoot/SoapWebService/SpringBootSoapWebService/SpringBootSoapWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.SoapWebService.SpringBootSoapWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSoapWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
