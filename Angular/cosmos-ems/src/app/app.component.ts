887cc26a7e2b82df75894617b191a164c1b24b5b
chamika94 <chamikasandaruwan1994@gmail.com>
2022-10-18 22:47:56 +0530

    Implemented function to get data to the home page from json file

--- Angular/cosmos-ems/src/app/app.component.ts
+++ Angular/cosmos-ems/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'em-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cosmos-Employee Management Solution';
+  title = 'Employee Management Solution';
 }
--- Angular/cosmos-ems/src/app/employees/employees.component.ts
+++ Angular/cosmos-ems/src/app/employees/employees.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import employees from './data/employees.json';
 
 @Component({
   selector: 'em-employees',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeesComponent implements OnInit {
 
+  title: string = 'Employee Management Solution';
+  showIcon: boolean = false;
+  employees: any[] = employees;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  showIcons(){
+    this.showIcon = !this.showIcon;
+  }
 }
